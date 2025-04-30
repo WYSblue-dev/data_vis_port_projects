02bd58e4f0b5b0f0b361e5e05f6b053529c9d596
William D. Jerrells iii <Willjerrells@gmail.com>
2025-04-30 17:07:59 -0400

    changed the yval starting position to imporve graph vis.

--- pay_graphing_pyplot/hourly_rate_scatter.py
+++ pay_graphing_pyplot/hourly_rate_scatter.py
@@ -14,14 +14,14 @@ fig, ax = plt.subplots()
 ax.scatter(range(len(rate_at_hour_data)), rate_at_hour_data, c=rate_at_hour_data,
            cmap=plt.cm.inferno)
 
-ax.axis([0, len(rate_at_hour_data), 0, rate_at_hour_data[-1]])
+ax.axis([0, len(rate_at_hour_data), pd.rate, rate_at_hour_data[-1]])
 
 ax.set_title(f"rate of wages {pd.rate}".upper(), fontsize=24, c='lime')
 ax.set_xlabel("hour".title(), fontsize=14, c='red')
 ax.set_ylabel("Value at hour".title(), fontsize=14, c='orange')
 
-ax.tick_params(labelcolor='black', labelsize=14, width=3,
-               size=5, color='purple', grid_color='black')
+ax.tick_params(labelcolor='black', labelsize=14, width=1,
+               size=3, color='purple', grid_color='black')
 # line caused the tick makes to be spaced by the int passed as indi but way too
 # many marks for readibility of the graph
 # ax.set_xticks(list(range(len(pay_data))))
